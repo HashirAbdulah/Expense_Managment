ccd7451b712278a0aea3411488acbe45eedb9d1d
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-11-11 00:14:29 +0500

    add username validation in register form

--- expensewebsite/expensewebsite/static/js/register.js
+++ expensewebsite/expensewebsite/static/js/register.js
@@ -1,6 +1,11 @@
 const usernameField = document.querySelector("#usernameField");
+const feedbackArea = document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField");
 usernameField.addEventListener("keyup", (e) => {
     const usernameVal = e.target.value;
+    usernameField.classList.add('is-invalid');
+    feedbackArea.style.display = 'block';
+    feedbackArea.innerText = '';
       fetch("/authentication/validate-username", {
         body: JSON.stringify({ username: usernameVal }),
         method: "POST",
@@ -8,6 +13,14 @@ usernameField.addEventListener("keyup", (e) => {
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
-            
+            if(data.error){
+              usernameField.classList.add('is-invalid');
+              feedbackArea.style.display = 'block';
+              feedbackArea.innerText = `${data.error}`;
+            }else {
+              usernameField.classList.remove("is-invalid");
+              feedbackArea.style.display = "none";
+            }
         });
   });
+
