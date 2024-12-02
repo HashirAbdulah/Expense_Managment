6c0289668d2cf95cc8f9f99205e8550c391ace3a
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-12-03 00:27:06 +0500

    "Added custom admin interface for Expense model"

--- expensewebsite/expenses/admin.py
+++ expensewebsite/expenses/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Expense, Category
 # Register your models here.
 
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount','date', 'description','owner','category')
+    search_fields = ('date','description','category')
+    list_per_page = 5
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
